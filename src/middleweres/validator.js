b6f7d07f10aae66542fcad241770ae0b060c4fc7
HigoJhon <higojhon98@gmail.com>
2023-03-14 19:42:23 -0300

    passando a mensagens para os campos nao correto

--- src/middleweres/validator.js
+++ src/middleweres/validator.js
@@ -1,20 +1,26 @@
 const isValidEmail = (req, res, next) => {
   const { email } = req.body;
   const pattern = /\S+@\S+\.\S+/;
+  
+  if (email === undefined) {
+    return res.status(400).json({ message: 'O campo \"email\" é obrigatório'});
+  }
+
   if (!pattern.test(email)) {
-    return res.status(422).json({
-      message: 'Invalid email',
-    });
+    return res.status(400).json({ message: 'O \"email\" deve ter o formato \"email@email.com\"' });
   }
   return next();
 };
 
 const isValidPassword = (req, res, next) => {
   const { password } = req.body;
+  
+  if (password === undefined) {
+    return res.status(400).json({ message: 'O campo \"password\" é obrigatório' });
+  }
+  
   if (password.length < 6) {
-    return res.status(422).json({
-      message: 'Password must contain at least 6 characters',
-    });
+    return res.status(400).json({ message: 'O \"password\" deve ter pelo menos 6 caracteres' });
   }
   return next();
 };
