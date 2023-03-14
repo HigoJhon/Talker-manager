1b3604cd380584df67fe0f0b05314eb322321eb6
HigoJhon <higojhon98@gmail.com>
2023-03-14 17:10:12 -0300

    buscando palestrantes cadastradas

--- src/index.js
+++ src/index.js
@@ -1,10 +1,19 @@
 const express = require('express');
+const path = require('path');
+const fs = require('fs').promises;
 
 const app = express();
 app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
+
+app.get('/talker', async (req, res) => {
+  const talker = await fs.readFile(path.resolve(__dirname, './talker.json'))
+  const data = JSON.parse(talker);
+  res.status(HTTP_OK_STATUS).json(data);
+});
+
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
