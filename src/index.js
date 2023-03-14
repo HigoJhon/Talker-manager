f4e8adf2553968cd06b954e0632873086ba0dd42
HigoJhon <higojhon98@gmail.com>
2023-03-14 18:25:47 -0300

    buscando pelo id

--- src/index.js
+++ src/index.js
@@ -8,12 +8,26 @@ app.use(express.json());
 const HTTP_OK_STATUS = 200;
 const PORT = process.env.PORT || '3001';
 
+const talkerRoute = './talker.json'
+
 app.get('/talker', async (req, res) => {
-  const talker = await fs.readFile(path.resolve(__dirname, './talker.json'))
+  const talker = await fs.readFile(path.resolve(__dirname, talkerRoute));
   const data = JSON.parse(talker);
   res.status(HTTP_OK_STATUS).json(data);
 });
 
+app.get('/talker/:id', async (req, res) => {
+  const { id } = req.params;
+  const talker = await fs.readFile(path.resolve(__dirname, talkerRoute));
+  const data = JSON.parse(talker);
+  const resul = data.find((a) => a.id === +id);
+  if (resul) {
+    return res.status(HTTP_OK_STATUS).json(resul);
+  };
+  return res.status(404).json({
+    message: "Pessoa palestrante não encontrada"
+  });
+});
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
